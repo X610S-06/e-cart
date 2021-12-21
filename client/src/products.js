1c15b08c9154d468df004e7937a7149f2aa8753b
Somanath1458 <37767811+Somanath1458@users.noreply.github.com>
2021-12-22 02:15:18 +0530

    add products

--- client/src/products.js
+++ client/src/products.js
@@ -2,36 +2,39 @@ const products = [
     {
         _id : '1',
         name : 'Airpods Wireless Bluetooth Headphones',
-        description: 'Bluetooth Technology'
+        image : '/images/airpods.jpg',
+        description: 'Bluetooth Technology',
         brand : 'Apple',
         category : 'Electronics',
         price : 99.9,
         countInstock : 3,
         rating : 4.5,
         numReviews : 4,
-    }
+    },
     {
         _id : '2',
         name : 'Airpods Wireless Bluetooth Headphones',
-        description: 'Bluetooth Technology'
+        image : '/images/alexa.jpg',
+        description: 'Bluetooth Technology',
         brand : 'Apple',
         category : 'Electronics',
         price : 99.9,
         countInstock : 3,
         rating : 4.5,
         numReviews : 4,
-    }
+    },
     {
         _id : '3',
         name : 'Airpods Wireless Bluetooth Headphones',
-        description: 'Bluetooth Technology'
+        image : '/images/camera.jpg',
+        description: 'Bluetooth Technology',
         brand : 'Apple',
         category : 'Electronics',
         price : 99.9,
         countInstock : 3,
         rating : 4.5,
         numReviews : 4,
-    }
+    },
 ]
 
 export default products

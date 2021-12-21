f7a7c1c863141232469d289a87fe7f221bcc8832
Somanath1458 <37767811+Somanath1458@users.noreply.github.com>
2021-12-22 00:02:09 +0530

    Update website

---
+++ client/src/products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        _id : '1',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        description: 'Bluetooth Technology'
+        brand : 'Apple',
+        category : 'Electronics',
+        price : 99.9,
+        countInstock : 3,
+        rating : 4.5,
+        numReviews : 4,
+    }
+    {
+        _id : '2',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        description: 'Bluetooth Technology'
+        brand : 'Apple',
+        category : 'Electronics',
+        price : 99.9,
+        countInstock : 3,
+        rating : 4.5,
+        numReviews : 4,
+    }
+    {
+        _id : '3',
+        name : 'Airpods Wireless Bluetooth Headphones',
+        description: 'Bluetooth Technology'
+        brand : 'Apple',
+        category : 'Electronics',
+        price : 99.9,
+        countInstock : 3,
+        rating : 4.5,
+        numReviews : 4,
+    }
+]
+
+export default products
